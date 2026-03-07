ab9da38ec9a5f525963f6b29eba5873f2e3db772
Jérôme Blondon <jerome@blondon.fr>
2026-03-07 15:35:32 +0100

    feat(shell): redesign startup splash with tomosona loading console

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,15 @@ import './assets/tailwind.css'
 import './assets/theme-adapters/editorjs.css'
 
 createApp(App).mount('#app')
+
+declare global {
+  interface Window {
+    __TOMOSONA_HIDE_SPLASH__?: () => void
+    __TOMOSONA_SPLASH_TIMER__?: number
+  }
+}
+
+window.clearInterval(window.__TOMOSONA_SPLASH_TIMER__)
+window.requestAnimationFrame(() => {
+  window.__TOMOSONA_HIDE_SPLASH__?.()
+})
